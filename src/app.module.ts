178ae5352daf9cd025fe3436e404280fd489d047
Penk <pongparn.c@deftdev.tech>
2024-12-05 11:14:27 +0700

    create module orders, customers and products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, CustomersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
